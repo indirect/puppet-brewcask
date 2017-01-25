0d26da49d67eab89ba1749bfa63ab487127014fb
André Arko <andre@arko.net>
2017-01-24 18:58:39 -0800

    Stop creating $HOMEBREW_ROOT
    Creating the homebrew root dir means a conflict with `puppet-homebrew`, since Puppet will only allow one declaration of `File[/usr/local]` per run. This change allows `puppet-brewcask` to be installed alongside `puppet-homebrew` again.

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,10 +12,6 @@ class brewcask {
 
   homebrew::tap { 'caskroom/cask': }
 
-  file { $cask_home:
-    ensure => directory
-  }
-
   # This prevents typing root password the first time a cask is installed
   file { $cask_room:
     ensure  => directory,
