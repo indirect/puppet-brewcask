d853d73f57c64255772ac6247da460aebf235987
Andre Arko <andre@arko.net>
2017-01-28 18:02:39 -0800

    fix spec for lack of directory

--- spec/classes/brewcask_spec.rb
+++ spec/classes/brewcask_spec.rb
@@ -9,7 +9,6 @@ describe 'brewcask' do
   end
 
   it do
-    should contain_file('/test/custom/homebrew-cask').with_ensure('directory')
     should contain_file('/test/custom/homebrew-cask/Caskroom').with_ensure('directory')
   end
 
